0098fe214f59bb4fa9767fc54eb157b09855487d
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-17 12:53:28 -0500

    Se realiza ejercicio 01(duplicar) del taller 2 metodos no mutables

---
+++ CamiloAndresRodriguezOquendo/script.js
@@ -0,0 +1,66 @@
+// let nombres = ["Camilo", "Andres", "Andrea", "Camila", "Gustavo", "Tatiana", "Andres"]
+// let resultadoUno = nombres.map(function(nombre){
+//     return nombre + " Rodriguez";
+// }) //Funcion regular
+// let resultadoDos = nombres.map((nombre)=> nombre + " Rodriguez") // Funcion Flecha
+
+// console.log(nombres);
+// console.log(resultadoDos);
+// console.log(resultadoDos);
+// resultadoDos.push("Otro nombre")
+// console.log(resultadoDos);
+
+// let resultadoFindUno = nombres.find(function(nombre){
+//     if(nombre == "Camilo"){
+//         return nombre
+//     }
+// })
+
+// let resultadoFindDos = nombres.find((nombre)=> nombre == "Camilo")
+
+// console.log(nombres);
+// console.log(resultadoFindDos);
+// console.log(resultadoFindDos);
+
+// let resultadofilter = nombres.filter((nombre)=> nombre == "Andres")
+// console.log(resultadofilter);
+
+// let resultadoSome = nombres.some((nombre)=> nombre == "Tatiana")
+// console.log(resultadoSome);
+
+// let resultadoEvery = nombres.every((nombre)=> nombre == "Gustavo")
+// console.log(resultadoEvery);
+
+// let resultadoIncludes = nombres.includes("Andrea")
+// console.log(resultadoIncludes);
+
+// let otroArreglo = [1, 2, 3, 4, 5, 6, 7, 8];
+// let resultadoconcat =nombres.concat(otroArreglo);
+// console.log(resultadoconcat);
+
+
+// let persona =[ 
+// {
+//     nombre: "Camilo",
+//     edad: 24,
+//     telefono: 3045624816,
+//     direccion: "calle 12#30-68"
+// }
+// {
+//     nombre: "Andres",
+//     edad: 24,
+//     telefono: 3045417784,
+//     direccion: "calle10-82-320"
+// }
+// ]
+
+// console.log(persona);
+// console.log(persona[1]);
+// console.log(persona[1].telefono);
+
+// Ejersicio 01
+let puntos = [10, 20, 30, 40]
+let puntosDobles = puntos.map(valor => valor*2)
+
+console.log(puntos);
+console.log(puntosDobles);
