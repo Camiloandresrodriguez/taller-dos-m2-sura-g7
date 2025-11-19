fa75e333e17e9780b97747c135467c4c9f19a8bc
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-18 19:53:55 -0500

    Se realiza ejercicio 04(Stock mayor a 0) del taller metodos no mutables

--- CamiloAndresRodriguezOquendo/script.js
+++ CamiloAndresRodriguezOquendo/script.js
@@ -17,3 +17,9 @@ let aprobadas = notas.filter((nota) => nota = nota >= 6)
 
 console.log(aprobadas);
 
+//Ejercicio 4 taller metodos no mutables.
+let productos = [{nombre: "Camisa", stock: 10}, {nombre: "Zapatos", stock: 0}, {nombre: "Pantalón", stock: 5}];
+let enStock = productos.filter((Stock) => Stock.stock > 0)
+
+console.log(enStock);
+
