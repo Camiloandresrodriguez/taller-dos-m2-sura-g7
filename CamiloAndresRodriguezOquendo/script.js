545d79c7db2d46ef81feef775499779ebae7906a
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-19 00:13:20 -0500

    Se realiza el ejercicio 20(Combinado) del taller de metodos no mutables

--- CamiloAndresRodriguezOquendo/script.js
+++ CamiloAndresRodriguezOquendo/script.js
@@ -134,6 +134,16 @@ let nuevoFeed = ["Noticia urgente"].concat(feed)
 
 console.log(nuevoFeed);
 
+//Ejercicio 19 taller metodos no mutables.
+let puntuaciones = [50, 85, -10, 100, 0, 30];
+let validas = puntuaciones.filter((puntaje) => puntaje >= 0)
+let bonificadas = validas.map(puntuacionValida => puntuacionValida+10)
+let totalPuntos = bonificadas.reduce((acumulador , valor) => {return acumulador+valor},0)
+
+console.log(validas);
+console.log(bonificadas);
+console.log(totalPuntos);
+
 
 
 
