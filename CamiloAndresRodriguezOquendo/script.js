3f1a22b633694f3f7e7c6d8f536133394f9c2e43
Camiloandresrodriguez <camirodoq88@gmail.com>
2025-11-18 23:55:37 -0500

    Se realiza el ejercicio 18(Producto caro) del taller de metodos no mutables

--- CamiloAndresRodriguezOquendo/script.js
+++ CamiloAndresRodriguezOquendo/script.js
@@ -122,6 +122,13 @@ let nuevoTablero = tablero.map((valor , indice) => {
 console.log(tablero);
 console.log(nuevoTablero);
 
+//Ejercicio 18 taller metodos no mutables.
+let productos2 = [{nombre: "Papas", precio: 2}, {nombre: "Carne", precio: 15}, {nombre: "Leche", precio: 3}];
+let productosCaros = productos2.filter((producto) => producto.precio > 4).map(producto => producto.nombre)
+
+console.log(productosCaros);
+
+
 
 
 
